7a39cef0a6d0ee1464fd3c952e41a8df5387ba39
ansonmak <ansonmak1997@gmail.com>
2022-01-20 17:45:12 +0800

    Tested urx local dir import

--- urx/__init__.py
+++ urx/__init__.py
@@ -7,6 +7,7 @@ __version__ = "0.11.0"
 
 try:
     from urx.robot import Robot
+    print('importing urx from /high_speed_scooping')
 except ImportError as ex:
     print("Exception while importing math3d base robot, disabling use of matrices", ex)
     Robot = URRobot
