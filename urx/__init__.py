d34224228e1dc471a6c5a537b02b3931d6c894b4
ansonmak <ansonmak1997@gmail.com>
2022-01-21 19:08:02 +0800

    update urx init

--- urx/__init__.py
+++ urx/__init__.py
@@ -6,8 +6,8 @@ from urx.urrobot import RobotException, URRobot  # noqa
 __version__ = "0.11.0"
 
 try:
-    from urx.robot import Robot
     print('importing urx from /high_speed_scooping')
+    from urx.robot import Robot
 except ImportError as ex:
     print("Exception while importing math3d base robot, disabling use of matrices", ex)
     Robot = URRobot
